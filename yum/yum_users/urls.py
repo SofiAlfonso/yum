c4790191c3a7e3e6d0e51b8eee819c14aa634b50
SofiAlfonso <asalfonsom@eafit.edu.co>
2025-09-07 22:43:11 -0500

    feat: delete view for  ingredient type entity

--- yum/yum_users/urls.py
+++ yum/yum_users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import HomeView, IngredientTypeCreateView, IngredientTypeListView, IngredientTypeUpdateView
+from .views import (HomeView, IngredientTypeCreateView, IngredientTypeListView, 
+                    IngredientTypeUpdateView, IngredientTypeDeleteView)
 
 urlpatterns = [
   path('home/', HomeView.as_view(), name='user_home'),
   path('ingredienttypes/add/', IngredientTypeCreateView.as_view(), name='ingredienttype_add'),
   path('ingredienttypes/', IngredientTypeListView.as_view(), name='ingredienttype_list'),
   path("ingredient-types/<int:pk>/edit/", IngredientTypeUpdateView.as_view(), name="ingredienttype_edit"),
+  path("ingredient-types/<int:pk>/delete/", IngredientTypeDeleteView.as_view(), name="ingredienttype_delete"),
+
 ]
--- yum/yum_users/views.py
+++ yum/yum_users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from core.models import Recipe, IngredientType
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import IngredientTypeForm
 from django.core.exceptions import PermissionDenied
@@ -71,6 +71,26 @@ class IngredientTypeUpdateView(UpdateView):
             raise PermissionDenied("No tienes permiso para editar este ingrediente.")
         return obj
     
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect(self.login_url)
+         
+        if request.user.role != "common":
+            return redirect(self.login_url) 
+        return super().dispatch(request, *args, **kwargs)
+
+
+class IngredientTypeDeleteView(DeleteView):
+    model = IngredientType
+    template_name = "yum_users/ingredient_type/confirm_delete.html"
+    success_url = reverse_lazy("ingredienttype_list")
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.user != self.request.user:
+            raise PermissionDenied("No tienes permiso para eliminar este ingrediente.")
+        return obj
+     
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect(self.login_url)
